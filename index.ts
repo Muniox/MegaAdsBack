707ac745ea6f1bed3e608a68dc5ab166603ca705
Muniox <muniox20@gmail.com>
2023-02-02 12:46:45 +0100

    Added error handling.

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import { ValidationExpressError, handleError } from "./utils/errors";
 import 'express-async-errors';
 
 const app = express();
@@ -10,7 +11,13 @@ app.use(cors({
 app.use(express.json());
 
 
+app.get('/', async (req, res) => {
+    throw new ValidationExpressError('Damn');
+});
+
+
+app.use(handleError);
 
 app.listen(3001, 'localhost', () => {
     console.log('Listening on port http://localhost:3001');
-})
+});
