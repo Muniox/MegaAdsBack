6f0c3f961d56ece1e712da1bc1ad12112f1f67ca
Muniox <muniox20@gmail.com>
2023-02-07 21:42:38 +0100

    add some validation to active record

--- rekords/ad.records.ts
+++ rekords/ad.records.ts
@@ -1,4 +1,5 @@
 import { AdEntity } from "../types";
+import {ValidationExpressError} from "../utils/errors";
 
 interface NewAdEntity extends Omit<AdEntity, 'id'> {
     id?: string;
@@ -13,7 +14,23 @@ export class AdRecord implements AdEntity {
     public lat: number;
     public lon: number;
     
-    constructor(obj: AdEntity) {
+    constructor(obj: NewAdEntity) {
+        if (!obj.name || obj.name.length > 100) {
+            throw new ValidationExpressError('Nazwa ogłoszenia nie może być pusta lub przekraczać 100 znaków.');
+        }
+        if (obj.description.length > 1024) {
+            throw new ValidationExpressError('Treść ogłoszenia nie może przekraczać 1000 znaków.')
+        }
 
+        if (obj.price < 0 || obj.price > 9999999) {
+            throw new ValidationExpressError('Cena nie może być mniejsza niż 0 lub większa niż 9 999 999.')
+        }
+        //@TODO: Check if URL is valid!
+        if(!obj.url || obj.url.length > 100) {
+            throw new ValidationExpressError('Adres ogłoszenia nie może być pusty, ani przekraczać 100 znaków.')
+        }
+        if (typeof obj.lat !=='number' || typeof obj.lon !== 'number') {
+            throw new ValidationExpressError('Nie można zlokalizować ogłoszenia')
+        }
     }
 }
